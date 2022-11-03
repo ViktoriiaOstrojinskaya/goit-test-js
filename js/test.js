7cc21263597ae9dc5016d0a9d6a7cecff32f53ef
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-11-03 12:00:57 -0400

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,12 +1,24 @@
-const numbers = [1,2,3,1,2,17,19]
+const numbers = [1, 1, 2, 3, 1, 2, 17, 1, 1, 1, 19, 1];
+
+// function getCommonElements(arr) {
+//   for (let i = 0; i < arr.length; i += 1) {
+//     const idx = arr.indexOf(arr[i], i + 1);
+//     if (idx !== -1) {
+//       arr.splice(idx, 1);
+//       i -= 1;
+//     }
+//   }
+//   return arr;
+// }
 
 function getCommonElements(arr) {
-    for (let i=0; i < arr.length; i +=1){
-        const idx = arr.indexOf(arr[i], i+1);
-        if (idx !== -1){
-            arr.splice(idx.1)
-        }
+  for (let i = arr.length - 1; i >= 0; i -= 1) {
+    const idx = arr.indexOf(arr[i], i + 1);
+    if (idx !== -1) {
+      arr.splice(idx, 1);
+      //i -= 1;
     }
-    return arr;
+  }
+  return arr;
 }
 console.log(getCommonElements(numbers));
