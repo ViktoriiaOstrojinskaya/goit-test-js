c9898e0765ab30978e729cd0cd77dd2a4ae835c6
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-11-03 13:17:16 -0400

    update

--- js/test.js
+++ js/test.js
@@ -1,24 +1,35 @@
-const numbers = [1, 1, 2, 3, 1, 2, 17, 1, 1, 1, 19, 1];
+// const numbers = [1, 1, 2, 3, 1, 2, 17, 1, 1, 1, 19, 1];
+
+// // function getCommonElements(arr) {
+// //   for (let i = 0; i < arr.length; i += 1) {
+// //     const idx = arr.indexOf(arr[i], i + 1);
+// //     if (idx !== -1) {
+// //       arr.splice(idx, 1);
+// //       i -= 1;
+// //     }
+// //   }
+// //   return arr;
+// // }
 
 // function getCommonElements(arr) {
-//   for (let i = 0; i < arr.length; i += 1) {
+//   for (let i = arr.length - 1; i >= 0; i -= 1) {
 //     const idx = arr.indexOf(arr[i], i + 1);
 //     if (idx !== -1) {
 //       arr.splice(idx, 1);
-//       i -= 1;
+//       //i -= 1;
 //     }
 //   }
 //   return arr;
 // }
+// console.log(getCommonElements(numbers));
 
-function getCommonElements(arr) {
-  for (let i = arr.length - 1; i >= 0; i -= 1) {
-    const idx = arr.indexOf(arr[i], i + 1);
-    if (idx !== -1) {
-      arr.splice(idx, 1);
-      //i -= 1;
-    }
-  }
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+  // Змініть код лише під цим рядком
+  const arr = [...list];
+  // Змініть код лише над цим рядком
   return arr;
 }
-console.log(getCommonElements(numbers));
+const arr = removeFirstTwo(source);
+console.log(arr);
+console.log(source);
