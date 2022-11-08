44347c7523f8718886c3ca3d9e852af018ca3e57
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-11-08 16:39:14 -0500

    ggg

--- js/test.js
+++ js/test.js
@@ -1,35 +1,16 @@
-// const numbers = [1, 1, 2, 3, 1, 2, 17, 1, 1, 1, 19, 1];
+document.querySelector(document).ready(function ($) {
+  (function initPlayVideo() {
+    const $videoCover = $('.f-video-cover');
+    const $videoPlayer = $('.f-video-player');
+    const $videoUrl = $('.f-video-cover').data('video');
 
-// // function getCommonElements(arr) {
-// //   for (let i = 0; i < arr.length; i += 1) {
-// //     const idx = arr.indexOf(arr[i], i + 1);
-// //     if (idx !== -1) {
-// //       arr.splice(idx, 1);
-// //       i -= 1;
-// //     }
-// //   }
-// //   return arr;
-// // }
-
-// function getCommonElements(arr) {
-//   for (let i = arr.length - 1; i >= 0; i -= 1) {
-//     const idx = arr.indexOf(arr[i], i + 1);
-//     if (idx !== -1) {
-//       arr.splice(idx, 1);
-//       //i -= 1;
-//     }
-//   }
-//   return arr;
-// }
-// console.log(getCommonElements(numbers));
-
-// const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// function removeFirstTwo(list) {
-//   // Змініть код лише під цим рядком
-//   const arr = [...list];
-//   // Змініть код лише над цим рядком
-//   return arr;
-// }
-// const arr = removeFirstTwo(source);
-// console.log(arr);
-// console.log(source);
+    $videoCover.on('click', function () {
+      $videoCover.fadeOut();
+      $videoPlayer.html(
+        '<iframe src="https://www.youtube.com/embed/' +
+          $videoUrl +
+          '?feature=oembed&autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+      );
+    });
+  })();
+});
